9f1860dc4da903726d8ca18e72ddd933a00e62b3
Emanuele Mazzucchi <emanuelemaz@outlook.it>
2023-08-11 01:26:49 +0200

    Render

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,9 @@ import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
+
+import node from '@sveltejs/adapter-node';
+
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
@@ -11,7 +14,10 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: node(),
+		methodOverride: {
+			allowed: ['PATCH', 'DELETE']
+		}
 	}
 };
 
