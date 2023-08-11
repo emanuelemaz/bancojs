68e6a324b8669ffb416af2519c0a132507b8d5f9
Emanuele Mazzucchi <emanuelemaz@outlook.it>
2023-08-11 15:49:36 +0200

    AppBar su piccoli schermi

---
+++ src/lib/Navigation.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { drawerStore } from '@skeletonlabs/skeleton';
+
+	function drawerClose(): void {
+		drawerStore.close();
+	}
+
+	$: homeActive = (href: string) => (href === $page.url.pathname ? '!bg-surface-600' : '');
+	$: classesActive = (href: string) => ($page.url.pathname.includes(href) && (href == "/" ? false : href != "/") ? '!bg-surface-600' : '');
+</script>
+
+<nav class="list-nav">
+	<ul>
+		<li><a on:click={drawerClose} href="/" class="{homeActive("/")}">Home</a></li>
+		<li><a on:click={drawerClose} href="/nuclei" class="{classesActive("/nuclei")}">Nuclei</a></li>
+		<li><a on:click={drawerClose} href="/bolle" class="{classesActive("/bolle")}">Bolle</a></li>
+		<li><a on:click={drawerClose} href="/alimenti" class="{classesActive("/alimenti")}">Alimenti</a></li>
+	</ul>
+</nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,21 +1,35 @@
-<script>
+<script lang="ts">
 	import '../app.postcss';
-	import { AppShell } from '@skeletonlabs/skeleton';
+	import { AppShell, AppBar, Drawer, drawerStore } from '@skeletonlabs/skeleton';
 	import '@skeletonlabs/skeleton/themes/theme-gold-nouveau.css';
 	import '@skeletonlabs/skeleton/styles/skeleton.css';
+	import Navigation from '$lib/Navigation.svelte';
+
+	function drawerOpen(): void {
+		drawerStore.open({});
+	}
 </script>
 
-<AppShell slotSidebarLeft="bg-surface-500/5 w-0 md:w-52 p-4">
+<Drawer class="g:w-0 w-64 "><Navigation /></Drawer>
+<AppShell slotSidebarLeft="bg-surface-500/5 w-0 lg:w-64 p-0 lg:p-4">
 	<svelte:fragment slot="sidebarLeft">
-		<nav class="list-nav">
-			<ul>
-				<li><a href="/">Home</a></li>
-				<li><a href="/nuclei">Nuclei</a></li>
-				<li><a href="/bolle">Bolle</a></li>
-				<li><a href="/alimenti">Alimenti</a></li>
-			</ul>
-		</nav>
+		<Navigation />
 	</svelte:fragment>
+	<AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
+		<svelte:fragment slot="lead">
+			<div class="flex items-center">
+				<button class="lg:hidden btn btn-sm mr-4" on:click={drawerOpen}>
+					<span>
+						<svg viewBox="0 0 100 80" class="fill-token w-4 h-4">
+							<rect width="100" height="20" />
+							<rect y="30" width="100" height="20" />
+							<rect y="60" width="100" height="20" />
+						</svg>
+					</span>
+				</button>
+			</div>
+		</svelte:fragment>
+	</AppBar>
 	<body data-theme="gold-nouveau">
 		<div class="container p-10 mx-auto">
 			<slot />
