bab84e74ce22ac218c88f706009fbea97434680a
Emanuele Mazzucchi <emanuelemaz@outlook.it>
2023-08-17 01:02:26 +0200

    Update +server.ts

--- src/routes/bolle/[slug]/pdf/+server.ts
+++ src/routes/bolle/[slug]/pdf/+server.ts
@@ -10,7 +10,6 @@ import moment from 'moment-timezone';
 export async function GET({ url, params }) {
 
     const displayNotes: boolean = url.searchParams.has("note")
-    const offset: number = parseInt(url.searchParams.get("offset") as string)
 
     const bolla = await prisma.bolla.findFirstOrThrow({
         where: {
@@ -104,7 +103,7 @@ export async function GET({ url, params }) {
             { text: [{ text: "Nucleo: " }, { text: "#" + nucleo.id, link: BASE_URL + "/nuclei/" + nucleo.id, font: 'Courier' }, { text: ` (${nucleo.nome} ${nucleo.cognome})` }], alignment: 'center', margin: [0, 2, 0, 0] },
             {
                 text: [{ text: "Bolla: " }, { text: "#" + bolla.id, link: BASE_URL + "/bolle/" + bolla.id, font: 'Courier' }, {
-                    text: " (" + moment(bolla.data).utcOffset(offset, true).format("DD/MM/YYYY, HH:mm:ss") + ")"
+                    text: " (" + moment(bolla.data).format("DD/MM/YYYY, HH:mm:ss") + ")"
                 }], alignment: 'center', margin: [0, 0, 0, 4]
             },
             {
