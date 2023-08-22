4cb940f78efd7d78fd332c609dbba3b9c010b60c
Emanuele Mazzucchi <emanuelemaz@outlook.it>
2023-08-22 16:01:50 +0200

    Aggiunta scadenza su lista alimenti

--- src/lib/Alimento.svelte
+++ src/lib/Alimento.svelte
@@ -1,50 +1,57 @@
 <script lang="ts">
-	import type { Alimento } from "@prisma/client";
-    export let row: Alimento;
+	import type { Alimento } from '@prisma/client';
+	import moment from 'moment-timezone';
+	export let row: Alimento;
 </script>
 
 <a class="block" href="/alimenti/{row.id}">
-    <div class="card card-hover mx-auto p-8">
-        {#if !row.distribuibile}
-            <div class="text-xl mb-2 chip variant-filled-surface">❌ Non distribuibile ❌</div>
-        {/if}
-        <div class="grid grid-cols-3 gap-4">
-            <div>
-                <p>Nome</p>
-                <p class="text-xl">
-                    {row.nome} (#{row.id})
-                </p>
-            </div>
-            <div>
-                <p>Unità</p>
-                <p class="text-xl">
-                    {row.unita}
-                </p>
-            </div>
-            <div>
-                <p>Prima scadenza</p>
-                <p class="text-xl">
-                    {#if row.scadenza}
-                        {row.scadenza?.toLocaleDateString('it-IT', {
-                            day: '2-digit',
-                            month: '2-digit',
-                            year: 'numeric'
-                        })}
-                    {:else}
-                        -
-                    {/if}
-                </p>
-            </div>
-        </div>
-        {#if row.note}
-            <!-- svelte-ignore a11y-invalid-attribute -->
-            <a href="#"
-                ><div class="mt-4">
-                    Note <textarea class="textarea p-2" readonly>{row.note}</textarea>
-                </div></a
-            >
-        {/if}
-    </div>
+	<div class="card card-hover mx-auto p-8">
+		{#if !row.distribuibile}
+			<div class="text-xl mb-2 chip variant-filled-surface">❌ Non distribuibile ❌</div>
+		{/if}
+		{#if row.scadenza && moment().isAfter(row.scadenza, 'day')}
+			<div class="text-xl mb-2 chip variant-filled-surface">⚠️ Scaduto ⚠️</div>
+		{/if}
+		{#if row.scadenza && moment().isSame(row.scadenza, 'day')}
+			<div class="text-xl mb-2 chip variant-filled-surface">⚠️ Scade oggi ⚠️</div>
+		{/if}
+		<div class="grid grid-cols-3 gap-4">
+			<div>
+				<p>Nome</p>
+				<p class="text-xl">
+					{row.nome} (#{row.id})
+				</p>
+			</div>
+			<div>
+				<p>Unità</p>
+				<p class="text-xl">
+					{row.unita}
+				</p>
+			</div>
+			<div>
+				<p>Prima scadenza</p>
+				<p class="text-xl">
+					{#if row.scadenza}
+						{row.scadenza?.toLocaleDateString('it-IT', {
+							day: '2-digit',
+							month: '2-digit',
+							year: 'numeric'
+						})}
+					{:else}
+						-
+					{/if}
+				</p>
+			</div>
+		</div>
+		{#if row.note}
+			<!-- svelte-ignore a11y-invalid-attribute -->
+			<a href="#"
+				><div class="mt-4">
+					Note <textarea class="textarea p-2" readonly>{row.note}</textarea>
+				</div></a
+			>
+		{/if}
+	</div>
 </a>
 
 <style>
